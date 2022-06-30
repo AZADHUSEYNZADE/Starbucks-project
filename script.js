d246810ec3db5f9cb06f10e93f7bbceba6c4dd0e
AZADHUSEYNZADE <89863498+AZADHUSEYNZADE@users.noreply.github.com>
2022-06-30 21:44:01 +0400

    firstCommit

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything) {
+  document.querySelector(".starbucks").src = anything;
+}
+function changeColor(color) {
+  const bgDiv = document.querySelector(".bgDiv");
+  bgDiv.style.background = color;
+}
